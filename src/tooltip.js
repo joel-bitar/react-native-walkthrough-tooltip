d79cae278ee316d983c5bee75d65b41721d1f54f
Benjamin Björklund <benjamin@hexigames.com>
2021-02-09 15:34:59 +0100

    componentWillReceiveProps > UNSAFE_componentWillReceiveProps

--- src/tooltip.js
+++ src/tooltip.js
@@ -99,7 +99,7 @@ class Tooltip extends Component {
     }
   }
 
-  componentWillReceiveProps(nextProps) {
+  UNSAFE_componentWillReceiveProps(nextProps) {
     const willBeVisible = nextProps.isVisible;
     const nextContent = nextProps.content;
     const { isVisible, content } = this.props;
